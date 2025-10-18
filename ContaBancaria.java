277c2b4564791eb7ab0d3da3f8030cf8e5c881f7
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-18 01:25:05 -0300

    Create ContaBancaria class for bank account operations

---
+++ ContaBancaria.java
@@ -0,0 +1,23 @@
+public class ContaBancaria {
+    private String numeroConta;
+    private double saldo;
+
+    public ContaBancaria(String numeroConta, double saldoInicial) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldoInicial;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+        System.out.println("Depósito de R$" + valor + " realizado. Saldo atual: R$" + saldo);
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado. Saldo atual: R$" + saldo);
+        } else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+}
