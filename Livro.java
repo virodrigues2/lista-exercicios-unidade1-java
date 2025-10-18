8cb766a3844ddf38f6a1e298f49079a2732eb10b
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-18 01:19:03 -0300

    Create Livro class with attributes and methods

---
+++ Livro.java
@@ -0,0 +1,17 @@
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+
+    public Livro(String titulo, String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public void exibirDetalhes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Páginas: " + numeroPaginas);
+    }
+}
