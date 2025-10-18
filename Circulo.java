a27261575e1d0ecd708337859482f7eef2752940
Vitória Mendes Rodrigues <vm40124@gmail.com>
2025-10-18 01:16:27 -0300

    Add Circulo class with area and perimeter methods

---
+++ Circulo.java
@@ -0,0 +1,21 @@
+public class Circulo {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * Math.PI * raio;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Raio: " + raio);
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro());
+    }
+}
